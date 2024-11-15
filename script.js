8b981fc986b37a27160d568f0e63d522c0ad6ac2
Pedro Moraes <pedromoraes005@gmail.com>
2024-11-15 01:49:34 -0300

    ajustes finais

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 import { dados } from "./videos.js";
 
-const API_KEY = "--";
+const API_KEY = "AIzaSyDvZQIdO1xC8LnJi5oh8j4SzrssJ4TAbd8";
 const MAX_RESULTADO = 200;
 const MAX_POR_PAGINA = 50;
 let videos = [];
@@ -16,23 +16,37 @@ const DIAS_DA_SEMANA = [
 ];
 
 function carregarCliente() {
-  // gapi.client.setApiKey(API_KEY);
-  // return gapi.client.load("youtube", "v3").then(
-  //   async function () {
-  console.log("YouTube API carregada");
-  videos = dados;
-  // const videosIds = await searchVideos("javascript");
-  console.log(videos);
-  //   },
-  //   function (error) {
-  //     console.error("Erro ao carregar a API", error);
-  //   }
-  // );
+  gapi.client.setApiKey(API_KEY);
+  return gapi.client.load("youtube", "v3").then(
+    function () {},
+    function (error) {
+      console.error("Erro ao carregar a API", error);
+    }
+  );
 }
 
-async function searchVideos(query, totalVideos = [], pageToken = "") {
-  if (totalVideos.length >= MAX_RESULTADO) {
-    return totalVideos;
+async function buscarNaAPI() {
+  const inputQuery = document.getElementById("valor-chave");
+  if (!inputQuery?.value) {
+    alert("Chave não informada");
+  } else {
+    const chave = substituirEspacosPorEComercial(inputQuery?.value);
+    const btnEnviar = document.getElementById("btnEnviar");
+    btnEnviar.disabled = true;
+    btnEnviar.textContent = "Carregando videos...";
+    videos = await buscarVideosRecursivo(chave);
+    btnEnviar.textContent = "Enviar tempo";
+    verificarCampos();
+  }
+}
+
+async function buscarVideosRecursivo(
+  query,
+  totalVideosDTO = [],
+  pageToken = ""
+) {
+  if (totalVideosDTO.length >= MAX_RESULTADO) {
+    return totalVideosDTO;
   }
 
   try {
@@ -46,12 +60,12 @@ async function searchVideos(query, totalVideos = [], pageToken = "") {
 
     const videoIds = response.result.items.map((item) => item.id.videoId);
 
-    const detailsResponse = await gapi.client.youtube.videos.list({
+    const videosDetalhados = await gapi.client.youtube.videos.list({
       part: "snippet,contentDetails",
       id: videoIds,
     });
 
-    const novosVideos = detailsResponse.result.items.map((item) => {
+    const videosDTO = videosDetalhados.result.items.map((item) => {
       return {
         id: item.id,
         tituloCanal: item.snippet.channelTitle,
@@ -61,14 +75,20 @@ async function searchVideos(query, totalVideos = [], pageToken = "") {
         url: `https://www.youtube.com/watch?v=${item.id}`,
       };
     });
-    totalVideos.push(...novosVideos);
+    totalVideosDTO.push(...videosDTO);
 
-    if (response.result.nextPageToken && totalVideos.length < MAX_RESULTADO) {
-      console.log("buscando");
-      await searchVideos(query, totalVideos, response.result.nextPageToken);
+    if (
+      response.result.nextPageToken &&
+      totalVideosDTO.length < MAX_RESULTADO
+    ) {
+      await buscarVideosRecursivo(
+        query,
+        totalVideosDTO,
+        response.result.nextPageToken
+      );
     }
 
-    return totalVideos;
+    return totalVideosDTO;
   } catch (error) {
     console.error("Erro ao buscar vídeos:", error);
   }
@@ -79,7 +99,6 @@ function formatarDuracao(duracao) {
   const hours = (match[1] || "0H").slice(0, -1);
   const minutes = (match[2] || "0M").slice(0, -1);
   const seconds = (match[3] || "0S").slice(0, -1);
-
   return `${hours.padStart(2, "0")}:${minutes.padStart(
     2,
     "0"
@@ -88,23 +107,19 @@ function formatarDuracao(duracao) {
 
 function converterParaSegundos(tempo) {
   const partes = tempo.split(":");
-
   const horas = parseInt(partes[0], 10) || 0;
   const minutos = parseInt(partes[1], 10) || 0;
   const segundos = parseInt(partes[2], 10) || 0;
-
   return horas * 3600 + minutos * 60 + segundos;
 }
 
 function verificarCampos() {
   const inputs = document.querySelectorAll('input[type="number"]');
   const botaoEnviar = document.getElementById("btnEnviar");
-
   const todosPreenchidos = Array.from(inputs).every(
     (input) => input.value !== ""
   );
-
-  botaoEnviar.disabled = !todosPreenchidos;
+  botaoEnviar.disabled = !todosPreenchidos || videos.length === 0;
 }
 
 function enviarFormulario(event) {
@@ -118,24 +133,35 @@ function enviarFormulario(event) {
     let tempoTotalVideos = 0;
     const videosParaAssistir = [];
     const tempoLivreNoDiaEmSegundos = tempo * 60;
-    if (index < videos.length) {
-      do {
+    do {
+      if (index < videos.length) {
         proximo = false;
         const video = videos[index];
-        const tempoVideo = converterParaSegundos(video.duracao);
-        if (tempoTotalVideos + tempoVideo < tempoLivreNoDiaEmSegundos) {
-          tempoTotalVideos += tempoVideo;
-          videosParaAssistir.push(video);
+        if (video?.duracao) {
+          const tempoVideo = converterParaSegundos(video.duracao);
+          if (tempoTotalVideos + tempoVideo < tempoLivreNoDiaEmSegundos) {
+            tempoTotalVideos += tempoVideo;
+            videosParaAssistir.push(video);
+            proximo = true;
+            index++;
+          }
+        } else {
           proximo = true;
           index++;
         }
-      } while (proximo);
-    }
+      } else {
+        proximo = false;
+      }
+    } while (proximo);
     videosNaSemana.push(videosParaAssistir);
   });
+  mostrarResultados(videosNaSemana);
+}
 
+function mostrarResultados(videosNaSemana) {
   const resultadoElement = document.getElementById("resultado");
-  resultadoElement.innerHTML = ""; // Limpa o conteúdo anterior
+  resultadoElement.innerHTML = "";
+
   videosNaSemana.forEach((videosDoDia, i) => {
     const diaDiv = document.createElement("div");
     diaDiv.classList.add("dia-videos");
@@ -179,6 +205,34 @@ function inicializarFormulario() {
   const formulario = document.querySelector("form");
   formulario.addEventListener("submit", enviarFormulario);
 }
+
+function alterarParaDadosMockados(event) {
+  const inputQuery = document.getElementById("valor-chave");
+  const btnEnviar = document.getElementById("btnEnviar");
+  const btnBuscar = document.getElementById("btnBuscar");
+  if (event.target.checked) {
+    videos = dados;
+    inputQuery.value = "javascript";
+    btnEnviar.textContent = "Enviar tempo";
+    btnBuscar.disabled = true;
+  } else {
+    videos = [];
+    inputQuery.value = "";
+    btnEnviar.textContent = "Videos não carregados";
+    btnBuscar.disabled = false;
+  }
+  verificarCampos();
+}
+
+function substituirEspacosPorEComercial(texto) {
+  return texto.replace(/ /g, "&");
+}
+
+document
+  .getElementById("checkbox-mock")
+  .addEventListener("change", alterarParaDadosMockados);
+
+document.getElementById("btnBuscar").addEventListener("click", buscarNaAPI);
 
 document.addEventListener("DOMContentLoaded", inicializarFormulario);
 
